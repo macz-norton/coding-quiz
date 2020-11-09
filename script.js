c136c80683f173c401f92b369a956aec1b27fdbd
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2020-11-09 09:33:03 -0800

    Added variables for the different IDs in the HTML

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
-var timerEl = document.querySelector("#timer");
-var startBtnEl = document.getElementById("start");
-var answerStatusEl = document.getElementById("answerStatus");
-var introEl = document.querySelector("#intro");
+// Display-specific variables
+var showEl = document.querySelector(".show");
+var hideEl = document.querySelector(".hide");
 
+// Variables for header
+var timerEl = document.getElementById("scoreTimer");
+
+// Variables for quiz intro
+var quizIntroEl = document.getElementById("quizIntro");
+var quizStartBtn = document.getElementById("quizStartBtn");
+
+// Variables for quiz in progress
+var quizInProgressEl = document.getElementById("quizInProgress");
+var questionTextEl = document.getElementById("questionText");
+var answer1Btn = document.getElementById("answer1");
+var answer2Btn = document.getElementById("answer2");
+var answer3Btn = document.getElementById("answer3");
+var answer4Btn = document.getElementById("answer4");
+var answerStatusEl = document.getElementById("answerStatus");
+
+// Variables for quiz completion
+var quizCompleteEl = dopcument.getElementById("quizComplete");
+var finalScoreEl = document.getElementById("finalScore");
+var initialsEl = document.getElementById("initials");
+var scoreSubmitBtn = document.getElementById("scoreSubmitBtn");
+
+// Variables for quiz leaderboard
+var leaderboardEl = document.getElementById("leaderboard");
+var playerScoresEl = document.getElementById("playerScores");
+var goBackBtn = document.getElementById("gotBackBtn");
+var clearScoresBtn = document.getElementById("clearScoresBtn");
+
+// Variables for timer and question index
 var secondsLeft = 100;
 var questionIndex = 0;
 
+//Variables for questions with answers and correct answers
 var questions = [
     {
         question: "What is the best color?",
         choices = ["red", "blue", "best", "purple"],
         correctAnswer = choices[2]
-    }
+    },
     {
         question: "What is the worst color?",
         choices = ["red", "worst", "green", "purple"],
         correctAnswer = choices[1]
-    }
+    },
     {
         question: "What is the coolest color?",
         choices = ["coolest", "blue", "green", "purple"],
